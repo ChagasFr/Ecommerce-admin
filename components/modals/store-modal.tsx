7446a2207fd683cd6d5f5f784449e68f5d69a255
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-05-22 16:28:59 -0300

    feat: forms

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -6,7 +6,9 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 import { useStoreModel } from "@/hooks/use-store-modal";
 import { Modal } from "@/components/ui/modal";
-import { Form, FormField, FormItem } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
 
 const formSchema = z.object({
     name: z.string().min(1)
@@ -42,10 +44,18 @@ export const StoreModal = () => {
                                 name="name"
                                 render={({ field }) => (
                                     <FormItem>
-
+                                        <FormLabel>Name</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="E-Commerce" {...field} />
+                                        </FormControl>
+                                        <FormMessage />
                                     </FormItem>
                                 )}
                             />
+                            <div className="pt-6 space-x-2 flex items-center justify-end w-full" >
+                                <Button variant="outline" onClick={storeModal.onClose}>Cancel</Button>
+                                <Button type="submit">Continue</Button>
+                            </div>
                         </form>
                     </Form>
                 </div>
