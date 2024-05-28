03ad825d51318fa07e19ed2855d109c3fa678eb5
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-05-28 18:46:50 -0300

    feat: settings form components

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -1,6 +1,9 @@
-import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation";
+
+import prismadb from "@/lib/prismadb";
+
+import { SettingsForm } from "./components/settings-form";
 
 interface SettingsPageProps {
     params: {
@@ -31,7 +34,7 @@ const SettingsPage: React.FC<SettingsPageProps> = async ({
     return (
         <div className="flex-col">
             <div className="flex-1 -space-y-4 p-8 pt-6">
-                Hello Settings
+                <SettingsForm initialData={store} />
             </div>
         </div>
     )
