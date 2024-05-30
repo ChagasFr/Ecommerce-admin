d394c0bd7a4e04dba4243a1884fe858aaf180669
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-05-29 22:23:10 -0300

    feat: components

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,7 +1,9 @@
 "use client"
+import { z } from "zod";
+import { useForm } from "react-hook-form";
 import { Trash } from "lucide-react";
 import { Store } from "@prisma/client";
-import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
@@ -12,11 +14,17 @@ interface SettingsFormProps {
 
 const formSchema = z.object({
     name: z.string().min(1)
-})
+});
+
+type SettingsFormValues = z.infer<typeof formSchema>
 
 export const SettingsForm: React.FC<SettingsFormProps> = ({
     initialData
 }) => {
+    const form = useForm<SettingsFormValues>({
+        resolver: zodResolver(formSchema)
+    })
+
     return (
         <>
             <div className="flex items-center justify-between">
