e96726b8ade8e584b6b31f57586df7f390b5b8cb
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-05-29 22:38:07 -0300

    feat: components

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -4,10 +4,12 @@ import { useForm } from "react-hook-form";
 import { Trash } from "lucide-react";
 import { Store } from "@prisma/client";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
 interface SettingsFormProps {
     initialData: Store;
 }
@@ -21,9 +23,17 @@ type SettingsFormValues = z.infer<typeof formSchema>
 export const SettingsForm: React.FC<SettingsFormProps> = ({
     initialData
 }) => {
+    const [open, setOpen] = useState(false);
+    const [loading, setLoading] = useState(false)
+
     const form = useForm<SettingsFormValues>({
-        resolver: zodResolver(formSchema)
+        resolver: zodResolver(formSchema),
+        defaultValues: initialData
     })
+
+    const onSubmit = async (data: SettingsFormValues) => {
+        console.log(data)
+    }
 
     return (
         <>
@@ -34,6 +44,21 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
                 </Button>
             </div>
             <Separator></Separator>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+                    <div className="grid grid-cols-3 gap-8">
+                        <FormField control={form.control} name="name" render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Name</FormLabel>
+                                <FormControl>
+                                    <Input disabled={loading} placeholder="store name" {...field} />
+                                </FormControl>
+                            </FormItem>
+                        )}
+                        />
+                    </div>
+                </form>
+            </Form>
         </>
     )
 }
