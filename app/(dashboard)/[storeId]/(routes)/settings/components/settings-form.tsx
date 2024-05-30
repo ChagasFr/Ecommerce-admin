92b47f87f1dc12ce3069ae54bedd929e9e3b5275
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-05-29 22:13:46 -0300

    feat: components

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,7 +1,9 @@
 "use client"
 
+import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Store } from "@prisma/client";
+import { Trash } from "lucide-react";
 interface SettingsFormProps {
     initialData: Store;
 }
@@ -11,8 +13,10 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
 }) => {
     return (
         <div className="flex items-center justify-between">
-
             <Heading title="Settings" description="Manage store preferences" />
+            <Button variant="destructive" size="sm" onClick={() => { }}>
+                <Trash className="h-4 w-4" />
+            </Button>
         </div>
     )
 }
