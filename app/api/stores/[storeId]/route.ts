fbf142d3d9076897992114fba5c41b27c45b227a
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-05-30 00:17:28 -0300

    feat: route api

--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -1,9 +1,21 @@
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
 export async function PATCH(
   req: Request,
   { params }: { params: { storeId: string } }
 ) {
   try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    const { name } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
   } catch (error) {
     console.log("[STORE_PATCH]", error);
+    return new NextResponse("interna error", { status: 500 });
   }
 }
