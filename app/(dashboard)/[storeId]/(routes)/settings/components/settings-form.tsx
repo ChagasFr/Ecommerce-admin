bf5ea5e8d091471e384ec4912a9ac68b2c266cee
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-05-31 16:04:09 -0300

    feat: form components

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -14,6 +14,7 @@ import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { AlertModal } from "@/components/modals/alert-modal";
 interface SettingsFormProps {
     initialData: Store;
 }
@@ -51,8 +52,24 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
         }
     }
 
+    const onDelete = async () => {
+        try {
+            setLoading(true)
+            await axios.delete(`/api/stores${params.storeId}`)
+            router.refresh();
+            router.push("/");
+            toast.success("Store updates");
+        } catch (error) {
+            toast.error("Make sure you removed all products and categories first")
+        } finally {
+            setLoading(false)
+            setOpen(false)
+        }
+    }
+
     return (
         <>
+            <AlertModal isOpen={open} onClose={() => setOpen(false)} onConfirm={onDelete} loading={loading} />
             <div className="flex items-center justify-between">
                 <Heading title="Settings" description="Manage store preferences" />
                 <Button disabled={loading} variant="destructive" size="sm" onClick={() => { }}>
