38068850ba2e152625e59bc1a8108b126a97396c
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-05-30 22:04:28 -0300

    fix: route api

--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -8,29 +8,19 @@ export async function DELETE(
 ) {
   try {
     const { userId } = auth();
-    const body = await req.json();
-
-    const { name } = body;
 
     if (!userId) {
       return new NextResponse("Unauthenticated", { status: 401 });
-    }
-
-    if (!name) {
-      return new NextResponse("Name is required", { status: 400 });
     }
 
     if (!params.storeId) {
       return new NextResponse("Name is required", { status: 400 });
     }
 
-    const store = await prismadb.store.updateMany({
+    const store = await prismadb.store.deleteMany({
       where: {
         id: params.storeId,
         userId,
-      },
-      data: {
-        name,
       },
     });
 
