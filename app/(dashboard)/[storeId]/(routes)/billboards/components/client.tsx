9f91afd1ba0062401847bb52753254bcf6b3dd28
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-06-01 21:44:24 -0300

    feat: billboard

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import { Heading } from "@/components/ui/heading";
+
 export const BillboardClient = () => {
     return (
         <>
             <div className="flex items-center justify-between">
-                BillboardClient
+                <Heading title="Billboards (0)" description="Manage billboards for your store" />
             </div>
         </>
     )
