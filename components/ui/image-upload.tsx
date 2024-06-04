42647f178080a5ce03e487c98b2ee8c0d169226d
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-06-04 00:02:01 -0300

    feat: imageup components

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 interface ImagemUpLoadProps {
     disabled?: boolean;
     onChange: (value: string) => void;
@@ -11,8 +15,31 @@ const ImagemUpLoad: React.FC<ImagemUpLoadProps> = ({
     onRemove,
     value
 }) => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    const onUpload = (result: any) => {
+        onChange(result.info.secure_url)
+    }
+
+    if (!isMounted) {
+        return null
+    }
 
     return (
-        <div>ImageUpload</div>
+        <div>
+            <div className="mb-4 flex items-center gap-4">
+                {value.map((url) => (
+                    <div key={url} className="relative w-[200-x] h-[200-x] rounded-md overflow-hidden">
+
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
+
+export default ImagemUpLoad
