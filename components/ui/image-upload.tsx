36b62adcaa0c556471deec0a1405eda39a3958cd
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-06-04 23:27:19 -0300

    Feat: ...

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -1,6 +1,11 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { Trash } from "lucide-react";
+import { Image } from "next/image";
+import { CldUploadWidget } from "next-cloudinary";
+
+import { Button } from "@/components/ui/button";
 
 interface ImagemUpLoadProps {
     disabled?: boolean;
@@ -34,10 +39,18 @@ const ImagemUpLoad: React.FC<ImagemUpLoadProps> = ({
             <div className="mb-4 flex items-center gap-4">
                 {value.map((url) => (
                     <div key={url} className="relative w-[200-x] h-[200-x] rounded-md overflow-hidden">
-
+                        <div className="z-10 absolute top-2 right-2">
+                            <Button type="button" onClick={() => onRemove(url)} variant="destructive" size="icon">
+                                <Trash className="h-4 2-4" />
+                            </Button>
+                        </div>
+                        <Image fill className="object-cover" alt="Image" scr={url} />
                     </div>
                 ))}
             </div>
+            <CldUploadWidget onUpload={onUpload} uploadWidget="">
+
+            </CldUploadWidget>
         </div>
     )
 }
