45c7f7c0ccb6092c9727703be927009f4f710d5e
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-06-07 00:26:56 -0300

    fix: ajuste route billboardId

--- app/api/[storeId]/billboards/[billboardId]/route.ts
+++ app/api/[storeId]/billboards/[billboardId]/route.ts
@@ -4,34 +4,18 @@ import { NextResponse } from "next/server";
 
 export async function GET(
   req: Request,
-  { params }: { params: { storeId: string; billboardId: string } }
+  { params }: { params: { billboardId: string } }
 ) {
   try {
     const { userId } = auth();
-
-    if (!userId) {
-      return new NextResponse("Unauthenticated", { status: 401 });
-    }
 
     if (!params.billboardId) {
       return new NextResponse("billboard is required", { status: 400 });
     }
 
-    const storeByUserId = await prismadb.store.findFirst({
-      where: {
-        id: params.storeId,
-        userId,
-      },
-    });
-
-    if (!storeByUserId) {
-      return new NextResponse("Unauthorized", { status: 403 });
-    }
-
-    const billboard = await prismadb.billboard.deleteMany({
+    const billboard = await prismadb.billboard.findUnique({
       where: {
         id: params.billboardId,
-        userId,
       },
     });
 
