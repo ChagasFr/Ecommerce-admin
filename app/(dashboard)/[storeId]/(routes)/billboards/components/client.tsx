2a32609dff8a3d4b7603c703a7e114891d222d90
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-06-07 23:38:54 -0300

    Feat: shadcn

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -8,17 +8,19 @@ import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 
 interface BillboardClientProps {
-    data: Billboard
+    data: Billboard[]
 }
 
-export const BillboardClient = () => {
+export const BillboardClient: React.FC<BillboardClientProps> = ({
+
+}) => {
     const router = useRouter
     const params = useParams
 
     return (
         <>
             <div className="flex items-center justify-between">
-                <Heading title="Billboards (0)" description="Manage billboards for your store" />
+                <Heading title={`Billboards (${data.length})`} description="Manage billboards for your store" />
 
                 <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
                     <Plus className="mr-2 h-4 w-4" />
