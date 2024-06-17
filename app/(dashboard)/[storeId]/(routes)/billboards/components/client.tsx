8433fa8b5c1b459f479f2b7ab701da0a674e7cff
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-06-17 19:54:53 -0300

    feat: api list components

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -5,9 +5,11 @@ import { useParams, useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
+import { DataTable } from "@/components/ui/data-table";
+
 
 import { BillboardColum, columns } from "./columns";
-import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
 
 interface BillboardClientProps {
     data: BillboardColum[]
@@ -31,6 +33,9 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({
             </div>
             <Separator />
             <DataTable searchKey="label" columns={columns} data={data} />
+            <Heading title="API" description="API calss for billboards" />
+            <Separator />
+            <ApiList />
         </>
     )
 }
