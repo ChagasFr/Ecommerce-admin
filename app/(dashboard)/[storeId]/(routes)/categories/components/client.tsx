b2c61ac3c291ec3023487a7a4b11d666ce3069a3
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-06-18 22:42:28 -0300

    feat: categories colums

--- app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -24,18 +24,18 @@ export const CategoryClient: React.FC<CategoryClientProps> = ({
     return (
         <>
             <div className="flex items-center justify-between">
-                <Heading title={`Billboards (${data.length})`} description="Manage billboards for your store" />
+                <Heading title={`Categories (${data.length})`} description="Manage categories for your store" />
 
-                <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
+                <Button onClick={() => router.push(`/${params.storeId}/categories/new`)}>
                     <Plus className="mr-2 h-4 w-4" />
                     Add New
                 </Button>
             </div>
             <Separator />
-            <DataTable searchKey="label" columns={columns} data={data} />
-            <Heading title="API" description="API calss for billboards" />
+            <DataTable searchKey="name" columns={columns} data={data} />
+            <Heading title="API" description="API calss for Categories" />
             <Separator />
-            <ApiList entityName="billboards" entityIdName="billboardId" />
+            <ApiList entityName="categories" entityIdName="categoryId" />
         </>
     )
 }
