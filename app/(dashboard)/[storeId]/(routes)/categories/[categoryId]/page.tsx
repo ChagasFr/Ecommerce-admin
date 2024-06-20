3fa40909c3a4d62ea4d74489248299a1d05f77b0
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-06-19 21:52:51 -0300

    feat: page categories

--- app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/page.tsx
@@ -1,23 +1,23 @@
 import prismadb from "@/lib/prismadb"
 
-import { BillboardForm } from "./components/billboard-form"
+import { CategoryForm } from "./components/category-form"
 
 const CategoryPage = async ({
     params
 
 }: {
-    params: { caregoryId: string }
+    params: { categoryId: string }
 }) => {
     const category = await prismadb.category.findUnique({
         where: {
-            id: params.caregoryId
+            id: params.categoryId
         }
     })
 
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
-                <BillboardForm initialData={category} />
+                <CategoryForm initialData={category} />
             </div>
         </div >
     )
