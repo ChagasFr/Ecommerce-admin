f65ddf9b6c3581f9f549dda6dfa0ce0ef6afaedf
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-06-28 00:07:57 -0300

    fix: products dashboard

--- app/(dashboard)/[storeId]/(routes)/products/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/columns.tsx
@@ -33,6 +33,24 @@ export const columns: ColumnDef<ProductColumn>[] = [
         header: "Price",
     },
     {
+        accessorKey: "category",
+        header: "Category",
+    },
+    {
+        accessorKey: "size",
+        header: "Size",
+    },
+    {
+        accessorKey: "color",
+        header: "Color",
+        cell: ({ row }) => (
+            <div className="flex items-center gap-x-2">
+                {row.original.color}
+                <div className="h-6 rounded-full border" />
+            </div>
+        )
+    },
+    {
         accessorKey: "createAt",
         header: "Date",
     },
