f03ea0c0afaf98ffb43a71f1c3059b9570c30549
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-06-28 01:13:39 -0300

    fix: products dashboard

--- app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -24,18 +24,18 @@ export const ProductClient: React.FC<ProductClientProps> = ({
     return (
         <>
             <div className="flex items-center justify-between">
-                <Heading title={`Billboards (${data.length})`} description="Manage billboards for your store" />
+                <Heading title={`Products (${data.length})`} description="Manage Products for your store" />
 
-                <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
+                <Button onClick={() => router.push(`/${params.storeId}/products/new`)}>
                     <Plus className="mr-2 h-4 w-4" />
                     Add New
                 </Button>
             </div>
             <Separator />
             <DataTable searchKey="label" columns={columns} data={data} />
-            <Heading title="API" description="API calss for billboards" />
+            <Heading title="API" description="API calss for Products" />
             <Separator />
-            <ApiList entityName="billboards" entityIdName="billboardId" />
+            <ApiList entityName="products" entityIdName="productId" />
         </>
     )
 }
