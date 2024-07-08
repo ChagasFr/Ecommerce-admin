b73ccc64f5a8fbb6b4bc89d307514c0cac548641
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-07-08 18:42:28 -0300

    fix: ajuste order component

--- app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -1,15 +1,11 @@
 "use client";
-import { Plus } from "lucide-react";
-import { useParams, useRouter } from "next/navigation";
+import { useParams } from "next/navigation";
 
-import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { DataTable } from "@/components/ui/data-table";
 
-
 import { OrderColumn, columns } from "./columns";
-import { ApiList } from "@/components/ui/api-list";
 
 interface OrderClientProps {
     data: OrderColumn[]
@@ -18,19 +14,11 @@ interface OrderClientProps {
 export const OrderClient: React.FC<OrderClientProps> = ({
     data
 }) => {
-    const router = useRouter
-    const params = useParams
-
     return (
         <>
-            <div className="flex items-center justify-between">
-                <Heading title={`Orders (${data.length})`} description="Manage orders for your store" />
-            </div>
+            <Heading title={`Orders (${data.length})`} description="Manage orders for your store" />
             <Separator />
-            <DataTable searchKey="label" columns={columns} data={data} />
-            <Heading title="API" description="API calss for orders" />
-            <Separator />
-            <ApiList entityName="orders" entityIdName="orderId" />
+            <DataTable searchKey="products" columns={columns} data={data} />
         </>
     )
 }
