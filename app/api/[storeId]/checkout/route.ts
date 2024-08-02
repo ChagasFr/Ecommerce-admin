acd40232360cfaddb2d519a4a10018f3d18feccd
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-08-02 17:58:27 -0300

    feat: route of checkout

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -1,4 +1,6 @@
+import prismadb from "@/lib/prismadb";
 import { NextResponse } from "next/server";
+import Stripe from "stripe";
 
 const corsHeaders = {
   "Acess-Control-Allow-Origin": "*",
@@ -14,5 +16,48 @@ export async function POST(
   req: ReadableStreamBYOBRequest,
   { params }: { params: { storeId: string } }
 ) {
-  const { productId } = await req.json();
+  const { productIds } = await req.json();
+
+  if (!productIds || productIds.length === 0) {
+    return new NextResponse("Product ids are required", { status: 400 });
+  }
+
+  const products = await prismadb.product.findMany({
+    where: {
+      id: {
+        in: productIds,
+      },
+    },
+  });
+
+  const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
+
+  products.forEach((product) => {
+    line_items.push({
+      quantity: 1,
+      price_data: {
+        currency: "USD",
+        product_data: {
+          name: product.name,
+        },
+        unit_amount: product.price.tdNumber() * 100,
+      },
+    });
+  });
+
+  const order = await prismadb.order.create({
+    data: {
+      storeId: params.storeId,
+      isPaid: false,
+      orderItems: {
+        create: productIds.map((productId: string) => ({
+          product: {
+            connect: {
+              id: productId,
+            },
+          },
+        })),
+      },
+    },
+  });
 }
