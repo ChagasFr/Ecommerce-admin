6aba08cfb89e9fc06d862fae94a2b5dc5fb29080
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-08-05 19:01:53 -0300

    feat: webhook route

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -60,4 +60,24 @@ export async function POST(
       },
     },
   });
+
+  const session = await stripe.checkout.session.create({
+    line_items,
+    mode: "payment",
+    billing_adress_collection: {
+      enable: true,
+    },
+    success_url: `${process.env.FRONTEND_STORE_URL}/cart?sucess=1`,
+    cancel_url: `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`,
+    metadata: {
+      orderId: order.id,
+    },
+  });
+
+  return NextResponse.json(
+    { url: session.url },
+    {
+      headers: corsHeaders,
+    }
+  );
 }
