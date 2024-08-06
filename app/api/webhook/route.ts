b40e6ebcab3af937b61e21c527ae0020f6f92806
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-08-06 18:04:51 -0300

    feat: route webhook

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,3 +1,4 @@
+import prismadb from "@/lib/prismadb";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
@@ -20,4 +21,46 @@ export async function POST(req: Request) {
 
     const session = event.data.object as Stripe.Checkout.Session;
     const adress = session?.customer_details?.address; 
+
+    const adressComponents = [
+        adress?.line1,
+        adress?.line2,
+        adress?.city,
+        adress?.state,
+        adress?.postal_code,
+        adress?.country,
+    ];
+
+    const addressString = adressComponents.filter((c) => c !== null).join(', ');
+
+    if (event.type === "checkout.session.completed") {
+        const order = await prismadb.order.update({
+            where: {
+                id: session?.metadata?.orderId,
+            },
+            data: {
+                isPaid: true,
+                adress: addressString,
+                phone: session?.customer_details?.phone
+            },
+            include: {
+                orderItems: true,
+            }
+        });
+
+        const productsIds = order.orderItems.map((orderItem) => orderItem.productId);
+
+        await prismadb.product.updateMany({
+            where: {
+                id: {
+                    in: [...productsIds]
+                }
+            },
+            data: {
+                isArchived: true
+            }
+        });
+    }
+
+    return new NextResponse(null)
 }
